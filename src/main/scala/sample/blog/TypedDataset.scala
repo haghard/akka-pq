7bfcab8fedcab6dcc1a79aea480605244862e646
haghard <haghard84@gmail.com>
2020-01-05 14:19:38 +0100

    various changes

--- src/main/scala/sample/blog/TypedDataset.scala
+++ src/main/scala/sample/blog/TypedDataset.scala
@@ -5,8 +5,9 @@ import com.datastax.driver.core.Row
 //The idea from https://youtu.be/BfaBeT0pRe0?list=PLbZ2T3O9BuvczX5j03bWMrMFzK5OAs9mZ
 object TypedDataset {
 
-  trait Column[T] {
-    self: T ⇒
+  //Schema type
+
+  trait Column[T <: Column[T]] { self: T ⇒
     def name: String
     def nonEmpty(row: Row): Boolean = !row.isNull(name)
   }
@@ -17,7 +18,7 @@ object TypedDataset {
   sealed trait ScoreD extends Column[ScoreD]
 
   object Id extends Id {
-    override val name = "_id"
+    override val name = "id"
   }
 
   object Name extends Name {
@@ -32,22 +33,45 @@ object TypedDataset {
     override val name = "score_d"
   }
 
-  case class TypedRow[T](row: Row) {
 
-    def project[A <: Column[A]](c: Column[A]): TypedRow[T with A] = {
-      assert(c.nonEmpty(row))
-      copy[T with A](row)
-    }
-
+  object TypedRow {
+    def id(row: Row): TypedRow[Id] = TypedRow[Id](rawRow)
   }
 
-  def func[T <: Id with Name](ds: TypedRow[T]): TypedRow[T with Score] = ???
+  final case class TypedRow[S](row: Row) {
+    def |@|[T <: Column[T]](c: Column[T]): TypedRow[S with T] =
+      project(c)
 
-  //def func2[T <: Id with Name with Score](ds: Dataset[T]): Dataset[T] = ???
+    def |+|[T <: Column[T]](c: Column[T]): TypedRow[S with T] =
+      project(c)
 
-  val row: Row = ???
-  val data = TypedRow[Id](row).project(Name).project(ScoreD)
+    /**
+     * Checks in runtime that the give column is there. Tracks types in compile time.
+     */
+    def project[T <: Column[T]](c: Column[T]): TypedRow[S with T] = {
+      assert(c.nonEmpty(row))
+      copy[S with T](row)
+    }
+  }
 
-  func(data)
-  //func2(data) //compile error
+  /**
+   * Needs a row with Id and Name fields
+   */
+  def transformation0[T <: Id with Name](ds: TypedRow[T]): TypedRow[T with Score] = ???
+
+  def transformation1[T <: Id with Name with Score](ds: TypedRow[T]): TypedRow[T] = ???
+
+  val rawRow: Row = ???
+  val data = TypedRow.id(rawRow) |@| Name |@| ScoreD
+  val data0 = TypedRow.id(rawRow) |+| Name |+| ScoreD
+
+  transformation0(data)
+
+  /*
+  Compilation error
+  found   : sample.blog.TypedDataset.TypedRow[sample.blog.TypedDataset.Id with sample.blog.TypedDataset.Name with sample.blog.TypedDataset.ScoreD]
+  [error]  required: sample.blog.TypedDataset.TypedRow[T]
+  */
+  //transformation1(data)
+
 }
